eb08d1ab6b97a38e43e46462f414c0992001c211
wuyang <chenwuyangcn@gmail.com>
2023-09-19 16:49:46 +0800

    update model load

--- README.md
+++ README.md
@@ -4,13 +4,18 @@
  How to use it:
  ```python
  import cav_mae
+ from PIL import Image
  model = cav_mae.load('ckpt_path') 
  
  img_size = 224
  audio_length = 512 # 5 seconds, should match with the audio_length in model checkpoint
  n_mels = 128
- audio_preprocess, visual_preprocess = cav_mae.visual_preprocess(img_size), cav_mae.audio_preprocess(audio_length,n_mels)
  
+ # audio_preprocess only used for eval mode,for train mode, you should manually mask it
+ visual_preprocess, audio_preprocess = cav_mae.visual_preprocess(img_size), cav_mae.audio_preprocess(audio_length,n_mels)
+ 
+ image = visual_preprocess(Image.open('test.png')).unsqueeze(0) # 1,3,img_size,img_size
+ audio = audio_preprocess('test.wav').unsqueeze(0) # 1,audio_length,n_mels
  
  # you should manually change the model mode
  model.eval()
--- cav_mae/cav_mae.py
+++ cav_mae/cav_mae.py
@@ -7,16 +7,33 @@ import PIL
 import torchaudio
 import numpy as np
 from .model import CAVMAE
-def build_model(state_dict: dict):
-    _audio_length = int(state_dict['module.pos_embed_a'].size(1)*2)
+def build_model(state_dict: dict, multi_gpu=True):
+    try:
+        _audio_length = int(state_dict['module.pos_embed_a'].size(1)*2) # if the model is trained on multi-gpu
+    except:
+        _audio_length = int(state_dict['pos_embed_a'].size(1)*2) # if the model is trained on single-gpu
     model = CAVMAE(audio_length=_audio_length,modality_specific_depth=11)
-    model.load_state_dict(state_dict)
+    if multi_gpu:
+        if isinstance(model, torch.nn.DataParallel) == False:
+            model = torch.nn.DataParallel(model)
+            model.load_state_dict(state_dict, strict=False)
+    else: # not recommand
+        new_state_dict = {}
+        for key in state_dict:
+            new_key = key.replace('module.','')
+            new_state_dict[new_key] = state_dict[key]
+        model.load_state_dict(new_state_dict, strict=False)
+    
     return model
+    
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")   
 
 def visual_preprocess(img_res):
     return T.Compose([
             T.Resize(img_res, interpolation=PIL.Image.BICUBIC),
             T.CenterCrop(img_res),
+            _convert_image_to_rgb,
             T.ToTensor(),
             T.Normalize(
                 mean=[0.4850, 0.4560, 0.4060],
@@ -48,7 +65,7 @@ def audio_preprocess(target_length, melbins):
 
             pt = np.random.randint(-p + 1)
             fbank = fbank[pt : pt + target_length, :]
-
+	fbank = torch.transpose(fbank, 0, 1)	
         return fbank
     return _wav2fbank
 
