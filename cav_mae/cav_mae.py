34c65f4ac7b8941665156328ec3e87170a83a867
wuyang <chenwuyangcn@gmail.com>
2023-11-19 11:59:48 +0800

    Update cav_mae.py

--- cav_mae/cav_mae.py
+++ cav_mae/cav_mae.py
@@ -29,7 +29,7 @@ def build_model(state_dict: dict, multi_gpu=True):
 def _convert_image_to_rgb(image):
     return image.convert("RGB")   
 
-def visual_preprocess(img_res):
+def visual_preprocess_bak(img_res):
     return T.Compose([
             T.Resize(img_res, interpolation=PIL.Image.BICUBIC),
             T.CenterCrop(img_res),
@@ -39,9 +39,17 @@ def visual_preprocess(img_res):
                 mean=[0.4850, 0.4560, 0.4060],
                 std=[0.2290, 0.2240, 0.2250]
             )]) 
+def visual_preprocess(img_res):
+    return  T.Compose([
+    T.Resize(img_res, interpolation=PIL.Image.BICUBIC),
+    T.CenterCrop(img_res),
+    _convert_image_to_rgb,
+    T.ToTensor(),
+    T.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+])
 
-def audio_preprocess(target_length, melbins):
-    def _wav2fbank(filename):
+def audio_preprocess(target_length=512, melbins=128,dataset_mean=-6.364, dataset_std=3.693):
+    def _wav2fbank_bak(filename):
         waveform, sr = torchaudio.load(filename)
         waveform = waveform - waveform.mean()
         try:
@@ -66,6 +74,35 @@ def audio_preprocess(target_length, melbins):
             pt = np.random.randint(-p + 1)
             fbank = fbank[pt : pt + target_length, :]
         fbank = torch.transpose(fbank, 0, 1)	
+        return fbank
+    def _wav2fbank(filename):
+        
+        waveform, sr = torchaudio.load(filename)
+        waveform = waveform - waveform.mean()
+
+        
+        try:
+            fbank = torchaudio.compliance.kaldi.fbank(waveform, htk_compat=True, sample_frequency=sr, use_energy=False, window_type='hanning', num_mel_bins=melbins, dither=0.0, frame_shift=10)
+        except:
+            fbank = torch.zeros([target_length, melbins]) + 0.01
+            print('there is a loading error')
+
+       
+        n_frames = fbank.shape[0]
+
+        p = target_length - n_frames
+
+        # cut and pad
+        if p > 0:
+            # ReflectionPad2d
+            m = torch.nn.ZeroPad2d((0, 0, 0, p)) # TODO 用border填充
+            fbank = m(fbank)
+        elif p < 0:
+            # fbank = fbank[0:target_length, :] # origin
+
+            pt = np.random.randint(-p + 1)
+            fbank = fbank[pt : pt + target_length, :]
+        fbank = (fbank - dataset_mean) / (dataset_std)
         return fbank
     return _wav2fbank
 
@@ -111,6 +148,18 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
         model.float()
     return model
 
+def compute_audio_image_similarity(model, audio_embs, image_embs):
+    model.eval()
+    audio_emb, img_emb = model.module.forward_feat(audio_embs, image_embs)
+    audio_emb = audio_emb.mean(dim=1)
+    img_emb = img_emb.mean(dim=1)
+    image_features = img_emb / img_emb.norm(dim=1, keepdim=True)
+    audio_features = audio_emb / audio_emb.norm(dim=1, keepdim=True)
+    logit_scale = torch.tensor(0.05).to(image_features.device)
+    # cosine similarity as logits
+    logit_scale = logit_scale.exp()
+    logits_per_image = logit_scale * image_features @ audio_features.t()
+    return logits_per_image
 
 if __name__ == '__main__':
     pass
